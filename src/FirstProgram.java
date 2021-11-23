16d9718370bc9809512852b7980217aec1d3407f
maheshlanghe <maheshlanghe1998@gmail.com>
2021-11-23 15:26:04 +0530

    this program is done by lead

---
+++ src/FirstProgram.java
@@ -0,0 +1,12 @@
+
+public class FirstProgram {
+
+	public static void main(String[] args)
+	{
+		System.out.println("this is git program ");
+		
+		System.out.println("this is in staging area");
+
+	}
+
+}
